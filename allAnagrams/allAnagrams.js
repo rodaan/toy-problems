2d554f2c15e63119b346e1082ad5fcd0a7d3cf7f
rodaan <rodaan.rabang@gmail.com>
2016-08-10 21:35:25 -0700

    Implement allAnagrams

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -13,5 +13,22 @@
   */
 
 var allAnagrams = function(string) {
-  // Your code here.
+  // create result array
+  var result = [];
+  // create inner function which determines the results
+  var createAnagram = function(stringLeft, anagramSoFar){
+	  if(stringLeft === ''){
+      result.push(anagramSoFar);
+	  }
+	  // iterate over length of string
+	  for(var i = 0; i < stringLeft.length; i++){
+	    // add selected character to StringSoFar
+	    createAnagram(stringLeft.slice(0, i) + stringLeft.slice(i + 1), anagramSoFar + stringLeft[i]);
+	  }
+  }
+  createAnagram(string, '');
+  return result;
 };
+
+var anagrams = allAnagrams('abc');
+console.log(anagrams);
