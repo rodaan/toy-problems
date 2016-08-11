1bc25f2688b2e360be9b8a6bdce7674bf439dac8
rodaan <rodaan.rabang@gmail.com>
2016-08-10 20:58:51 -0700

    Implement asyncMap

--- asyncMap/asyncMap.js
+++ asyncMap/asyncMap.js
@@ -40,4 +40,46 @@
 
 
 var asyncMap = function(tasks, callback){
+	// create result array
+  var resultArray = [];
+	// create variable for keeping track of results
+  var resultCount = 0;
+	// iterate through array
+  for(var i = 0; i < tasks.length; i++){
+	  // create wrapper function that instantly invokes task on each iteration
+    (function(i){
+	  // pass in callback function into each task
+      tasks[i](function(val){
+	    // callback assigns task result into array
+      	resultArray[i] = val;
+	    // increment variable for results
+      	resultCount += 1;
+	    // if result number is equal to task array length
+      	if(resultCount === tasks.length){
+	      // invoke callback function on result array
+      		callback(resultArray)
+      	}
+      })
+    })(i)
+  }
 };
+
+// TEST CASE: 
+asyncMap([
+  function(cb){
+    setTimeout(function(){
+      cb('one');
+    }, 200);
+  },
+  function(cb){
+    setTimeout(function(){
+      cb('two');
+    }, 100);
+  }
+  ],
+  function(results){
+    // the results array will equal ['one','two'] even though
+    // the second function had a shorter timeout.
+    console.log(results); // ['one', 'two']
+ });
+
